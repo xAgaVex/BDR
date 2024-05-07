b91bdaa447b15712d03a8a8fd876da9393e3ecf1
xAgaVex <aleksander.rubacha@gmail.com>
2024-05-07 04:30:18 +0200

    Kupowanie productu

--- tests/removeProductFromCart.spec.ts
+++ tests/removeProductFromCart.spec.ts
@@ -2,29 +2,26 @@ import { test, expect } from '@playwright/test';
 import { AddToCart } from '../pageObjects/addToCart'
 import { LetsViewCart } from '../pageObjects/viewCart';
 import { RemoveProdFromCart } from '../pageObjects/removeFromCart';
+import { MenuPage} from '../pageObjects/menuPage';
+import { ProductsPage } from '../pageObjects/productsPage';
 
 let removeProdFromCart: RemoveProdFromCart
 let addToCart: AddToCart
 let letsViewCart: LetsViewCart
+let menu: MenuPage;
+let product: ProductsPage
 
-async function goToMenu(page, menuLink: string, menuName: string){
-    await page.getByRole('link', { name: menuLink }).click();
-    await expect(page.getByRole('link', { name: menuName, exact: true })).toBeVisible();
-}
-async function choseProduct(page, productLink: string, productName: string, size: string) {
-    await page.getByRole('link', { name: productLink }).click();
-    await expect(page.getByRole('heading', { name: productName })).toBeVisible();
-    await page.getByLabel(size, { exact: true }).nth(1).click();
-}
-
-
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://spree-multi-vendor-demo.herokuapp.com/');
+    addToCart = new AddToCart(page);
+    letsViewCart = new LetsViewCart(page);
+    removeProdFromCart = new RemoveProdFromCart(page);
+    menu = new MenuPage(page);
+    product = new ProductsPage(page);
+})
 test('usunięcie produktu z koszyka', async ({ page }) => {
-  addToCart = new AddToCart(page);
-  letsViewCart = new LetsViewCart(page);
-  removeProdFromCart = new RemoveProdFromCart(page);
-  await page.goto('https://spree-multi-vendor-demo.herokuapp.com/');
-  await goToMenu(page, 'Ambiance Men', 'Men');
-  await choseProduct(page, 'Denim Shirt Denim Shirt $', 'Denim Shirt', 'L');
+  await menu.goToMenu('Ambiance Men', 'Men');
+  await product.choseProduct('Denim Shirt Denim Shirt $', 'Denim Shirt', 'L');
   await addToCart.addToCart();
   await letsViewCart.LetsViewCart();
   await removeProdFromCart.RemoveProdFromCart();
